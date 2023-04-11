1b698d9362298af0f6ca3a0995e549709512b342
Alexander Bacomo <xandrebacomo@gmail.com>
2023-04-11 23:57:12 +0800

    s

--- js/app.js
+++ js/app.js
@@ -1,96 +1,121 @@
 gsap.registerPlugin(ScrollTrigger, ScrollToPlugin);
 
 let tl = gsap.timeline({
-	defaults: {
-		ease: "power4.inOut",
-		duration: 2,
-	},
+  defaults: {
+    duration: 2.5,
+    ease: "circ.out",
+    // stagger: 0.2,
+  },
 });
-tl.to("#flower1", { scale: 5, x: 500, opacity: 1, duration: 2 })
-	.to(
-		"#flower2",
-		{
-			scale: 3,
-			x: 200,
-			// rotate: 360,
-			opacity: 1,
-			// duration: 1,
-		},
-		"-=2",
-	)
-	.to(
-		"#flower3",
-		{
-			scale: 2,
-			x: 400,
-			// rotate: 360,
-			opacity: 1,
-			// duration: 2,
-		},
-		"=-2",
-	)
-	.to(
-		"#flower4",
-		{
-			scale: 3,
-			x: 600,
-			rotate: 360,
-			opacity: 1,
-			// duration: 2,
-		},
-		"=-2",
-	)
-	.from(
-		"#flower5",
-		{
-			scale: 2,
-			x: 100,
-			rotate: 360,
-			opacity: 0,
-			// duration: 2,
-			repeat: 1,
-		},
-		"=-2",
-	)
-	.to(
-		"h1",
-		{
-			"clip-path": "polygon(0% 100%, 100% 100%, 100% 0%,0% 0%)",
-			opacity: 1,
-			y: 0,
-		},
-		"-=2",
-	);
+tl.to("#flower1", {
+  scale: 4,
+  x: 100,
+  y: 100,
+  //   y: Math.random() * 400,
+  rotation: 360,
+  opacity: 1,
+  duration: 2,
+})
+  .to(
+    "#flower2",
+    {
+      scale: 4,
+      x: 200,
+      y: 20,
+      //   y: Math.random() * 300,
+      rotate: 360,
+      opacity: 1,
+    },
+    "-=1"
+  )
+  .to(
+    "#flower3",
+    {
+      scale: 4,
+      //   x: Math.random() * 200,
+      y: Math.random() * 300,
+      rotate: 360,
+      opacity: 1,
+      // duration: 2,
+    },
+    "=-2"
+  )
+  .to(
+    "#flower4",
+    {
+      scale: 2,
+      x: Math.random() * 200,
+      //   y: Math.random() * 300,
+      //   rotate: 360,
+      opacity: 1,
+      // duration: 2,
+    },
+    "=-2"
+  )
+  .to(
+    "#flower5",
+    {
+      scale: 2,
+      x: 100,
+      rotate: 360,
+      opacity: 1,
+      // duration: 2,
+      //   repeat: 1,
+    },
+    "=-2"
+  )
+  .to(
+    "h1",
+    {
+      "clip-path": "polygon(0% 100%, 100% 100%, 100% 0%,0% 0%)",
+      opacity: 1,
+      y: 0,
+    },
+    "-=2"
+  );
 
 gsap.to(".flipbook-wrap", {
-	ease: "back",
-	// y: -100,
-	duration: 3,
-	// ScrollTrigger: {
-	// 	trigger: ".flipbook-wrap",
-	// 	start: "top center",
-	// 	markers: true,
-	// },
+  ease: "back",
+  // y: -100,
+  duration: 3,
+  // ScrollTrigger: {
+  // 	trigger: ".flipbook-wrap",
+  // 	start: "top center",
+  // 	markers: true,
+  // },
 });
 
 let sections = document.querySelectorAll("section");
-mb;
 let links = document.querySelectorAll("header nav a");
 
 window.onscroll = () => {
-	sections.forEach((sec) => {
-		let top = window.scrollY;
-		let offset = sec.offsetTop - 150;
-		let height = sec.offsetHeight;
-		let id = sec.getAttribute("id");
+  sections.forEach((sec) => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 150;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute("id");
 
-		if (top >= offset && top <= offset + height) {
-			links.forEach((link) => {
-				link.classList.remove("active");
-				document
-					.querySelector('header nav a[href*="' + id + '"]')
-					.classList.add("active");
-			});
-		}
-	});
+    if (top >= offset && top <= offset + height) {
+      links.forEach((link) => {
+        link.classList.remove("active");
+        document
+          .querySelector('header nav a[href*="' + id + '"]')
+          .classList.add("active");
+      });
+    }
+  });
 };
+{
+  const nav = document.querySelector(".nav");
+  let lastScrollY = window.scrollY;
+
+  window.addEventListener("scroll", () => {
+    if (lastScrollY < window.scrollY) {
+      nav.classList.add("nav--hidden");
+    } else {
+      nav.classList.remove("nav--hidden");
+    }
+
+    lastScrollY = window.scrollY;
+  });
+}
